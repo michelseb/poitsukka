5084e96933e71ccb818445bb74b40e12016d2c4c
michelseb <sebastien.michel60@gmail.com>
2017-12-13 18:12:05 +0100

    Jump functionality

---
+++ Poitsukka/Transition.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Transition : MonoBehaviour {
+
+    Poitsukka poitsukka;
+    public Animator anim;
+
+	// Use this for initialization
+	void Start () {
+        poitsukka = GameObject.Find("Poitsukka").GetComponent<Poitsukka>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        anim.SetTrigger("jump");
+        Debug.Log("Saute !");
+    }
+}
