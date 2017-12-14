a051cedd964edb14f8a21e87a85add726bef7ca4
michelseb <sebastien.michel60@gmail.com>
2017-12-14 18:14:37 +0100

    TouchInput direction + sprite manager
    Management of sprites display order + work made on player direction

---
+++ Poitsukka/spriteManager.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class spriteManager : MonoBehaviour {
+
+    SpriteRenderer[] parts;
+    int[] partsOrder;
+    public GameObject mainPart;
+    int mainPartOrder;
+
+	// Use this for initialization
+	void Start () {
+        mainPartOrder = mainPart.GetComponent<SpriteRenderer>().sortingOrder;
+        parts = transform.GetComponentsInChildren<SpriteRenderer>();
+        partsOrder = new int[parts.Length];
+        for(int i = 0; i < parts.Length; i++)
+        {
+            partsOrder[i] = parts[i].sortingOrder - mainPartOrder;
+        }
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (mainPartOrder != mainPart.GetComponent<SpriteRenderer>().sortingOrder)
+        {
+            Debug.Log("Ordering!");
+            mainPartOrder = mainPart.GetComponent<SpriteRenderer>().sortingOrder;
+            Order();
+            
+        }
+		
+	}
+
+    public void Order()
+    {
+        for (int i = 0; i < parts.Length; i++)
+        {
+            parts[i].sortingOrder = mainPartOrder + partsOrder[i];
+        }
+    }
+}
